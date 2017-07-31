0b70a9ed436a4aff8321590bb43bd80b680ae036
flyblue <838797517@qq.com>
2017-07-31 19:45:35 +0800

    service单例模式demo

--- src/com/neusoft/bbs/service/impl/UserServiceImpl.java
+++ src/com/neusoft/bbs/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.neusoft.bbs.service.impl;
 
+import com.neusoft.bbs.commons.util.db.TransactionProxy;
 import com.neusoft.bbs.dao.UserDao;
 import com.neusoft.bbs.dao.impl.UserDaoImpl;
 import com.neusoft.bbs.domain.User;
@@ -10,6 +11,24 @@ import com.neusoft.bbs.service.UserService;
  *
  */
 public class UserServiceImpl implements UserService{
+	private static final TransactionProxy transctionProxy = new TransactionProxy();
+	/**
+	 * 类实例
+	 */
+	private static final UserService instance = (UserService) transctionProxy.newProxyInstance(new UserServiceImpl());
+
+	/**
+	 * 取得实例
+	 */
+	public static UserService getInstance() {
+		return instance;
+	}
+
+	/**
+	 * 构造方法
+	 */
+	private UserServiceImpl() {
+	}
 
 	@Override
 	public boolean login(String username, String password) {
