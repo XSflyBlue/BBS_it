1c4397234d48e717ebca82ecfe15baca01819c18
flyblue <838797517@qq.com>
2017-08-02 21:14:28 +0800

    继续填坑，增加对单个bean，Short类型属性的支持

--- src/com/neusoft/bbs/commons/util/db/BeanHandler.java
+++ src/com/neusoft/bbs/commons/util/db/BeanHandler.java
@@ -40,6 +40,8 @@ public class BeanHandler implements ResultSetHandler {
                     		 f.set(bean, bgDecimal.intValue());
                     	}else if(f.getType()==Double.class) {
                     		 f.set(bean, bgDecimal.doubleValue());
+                    	}else if(f.getType()==Short.class) {
+                    		f.set(bean, bgDecimal.shortValue());
                     	}
                     }else {
                     	f.set(bean, coulmnData);
