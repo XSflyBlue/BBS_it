feb65ebcfd861b3c5ed583b4d104031e84ac3d1b
cappuccinoqi <617674934@qq.com>
2017-08-02 17:20:24 +0800

    SectionDaoImpl和UserDetailDaoImpl实现类

--- src/com/neusoft/bbs/dao/impl/SectionDaoImpl.java
+++ src/com/neusoft/bbs/dao/impl/SectionDaoImpl.java
@@ -1,5 +1,6 @@
 package com.neusoft.bbs.dao.impl;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.neusoft.bbs.commons.util.db.BeanHandler;
@@ -7,18 +8,24 @@ import com.neusoft.bbs.commons.util.db.BeanListHandler;
 import com.neusoft.bbs.commons.util.db.DatabaseUtil;
 import com.neusoft.bbs.dao.SectionDao;
 import com.neusoft.bbs.domain.Section;
+import com.neusoft.bbs.domain.form.FollowForm;
+import com.neusoft.bbs.domain.form.PageForm;
+import com.neusoft.bbs.domain.form.SectionForm;
+
 /**
  * 板块Dao实现类
+ * 
  * @author yangmiao
  *
  */
-public class SectionDaoImpl implements SectionDao{
+public class SectionDaoImpl implements SectionDao {
 
 	@Override
 	public int insert(Section section) {
 		int a = 0;
 		String sql = "insert into b_section values(B_SECTION_ID_SEQ.nextval,?,?,?,?)";
-		Object params[] = {section.getSectionName(),section.getIsShow(),section.getDistrictId(),section.getSectionDescri()};
+		Object params[] = { section.getSectionName(), section.getIsShow(), section.getDistrictId(),
+				section.getSectionDescri() };
 		try {
 			a = DatabaseUtil.update(sql, params);
 		} catch (Exception e) {
@@ -31,7 +38,7 @@ public class SectionDaoImpl implements SectionDao{
 	public int delete(Section section) {
 		int a = 0;
 		String sql = "delete from b_section where section_id=?";
-		Object params[] = {section.getSectionId()};
+		Object params[] = { section.getSectionId() };
 		try {
 			a = DatabaseUtil.update(sql, params);
 		} catch (Exception e) {
@@ -44,8 +51,8 @@ public class SectionDaoImpl implements SectionDao{
 	public int update(Section section) {
 		int a = 0;
 		String sql = "update b_section set section_name=?,is_show=?,district_id=?,section_descri=? where section_id=?";
-		Object params[] = {section.getSectionName(),section.getIsShow(),section.getDistrictId(),
-							section.getSectionDescri(),section.getSectionId()};
+		Object params[] = { section.getSectionName(), section.getIsShow(), section.getDistrictId(),
+				section.getSectionDescri(), section.getSectionId() };
 		try {
 			a = DatabaseUtil.update(sql, params);
 		} catch (Exception e) {
@@ -58,10 +65,10 @@ public class SectionDaoImpl implements SectionDao{
 	public Section findByPostId(Long sectionId) {
 		int a = 0;
 		String sql = "select * from b_section where section_id=?";
-		Object params[] = {sectionId};
+		Object params[] = { sectionId };
 		Section section = null;
 		try {
-			section = (Section)DatabaseUtil.query(sql, params, new BeanHandler(Section.class));
+			section = (Section) DatabaseUtil.query(sql, params, new BeanHandler(Section.class));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -72,9 +79,9 @@ public class SectionDaoImpl implements SectionDao{
 	public List<Section> findDistrictAll(Long districtId) {
 		List<Section> list = null;
 		String sql = "select * from b_section where district_id=?";
-		Object params[] = {districtId};
+		Object params[] = { districtId };
 		try {
-			list = (List<Section>)DatabaseUtil.query(sql, params, new BeanListHandler(Section.class));
+			list = (List<Section>) DatabaseUtil.query(sql, params, new BeanListHandler(Section.class));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -87,11 +94,78 @@ public class SectionDaoImpl implements SectionDao{
 		String sql = "select * from b_section ";
 		Object params[] = {};
 		try {
-			list = (List<Section>)DatabaseUtil.query(sql, params, new BeanListHandler(Section.class));
+			list = (List<Section>) DatabaseUtil.query(sql, params, new BeanListHandler(Section.class));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return list;
+	}
+
+	@Override
+	public int getListPageCount(int pageSize, Section section) {
+		int res = 0;
+		int rowCount = getListRowCount(section);
+		if (rowCount % pageSize == 0) {
+			res = rowCount / pageSize;
+		} else {
+			res = rowCount / pageSize + 1;
+		}
+		return res;
+	}
+
+	@Override
+	public int getListRowCount(Section section) {
+		StringBuffer find_sql = new StringBuffer();
+		find_sql.append("SELECT count(*) ROW_COUNT ");
+		Long id = null;
+		// 参数确定
+		if (section.getSectionId() != null) {
+			id = section.getSectionId();
+			find_sql.append("FROM B_SECTION ");
+			find_sql.append("WHERE SECTION_ID=?");
+		} else {
+			return 0;
+		}
+		Object params[] = { id };
+		PageForm pageForm = null;
+		try {
+			pageForm = (PageForm) DatabaseUtil.query(find_sql.toString(), params, new BeanHandler(PageForm.class));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pageForm.getRowCount().intValue();
+	}
+
+	@Override
+	public List<SectionForm> findFormList(int pageSize, int rowNum, Section section) {
+		StringBuffer find_sql = new StringBuffer();
+		Long id = null;
+		// 参数确定
+		if (section.getSectionId() != null) {
+			id = section.getSectionId();
+			find_sql.append(
+					"SELECT S.SECTION_ID,S.SECTION_NAME,S.IS_SHOW,S.DISTRICT_ID,S.SECTION_DESCRI,COUNT (M.USER_ID) ADMIN_NUM");
+			find_sql.append(" FROM B_SECTION S,B_MODERATOR M");
+			find_sql.append(" WHERE S.SECTION_ID = 1");
+			find_sql.append(" AND M .MODERATOR_TYPE = 0");
+			find_sql.append(" AND M .AREA_ID = S.SECTION_ID");
+			find_sql.append(" GROUP BY S.SECTION_ID,SECTION_NAME,S.IS_SHOW,S.DISTRICT_ID,S.SECTION_DESCRI;");
+		} else {
+			return null;
+		}
+		// 分页SQL语句
+		String sql = "select*from (select a1.*,rownum rn from (" + find_sql.toString() + ") a1 where rownum<="
+				+ rowNum * pageSize + ") where rn>" + ((rowNum - 1) * pageSize);
+
+		System.out.println(sql);
+		Object params[] = { id };
+		List<SectionForm> sectionFormList = null;
+		try {
+			sectionFormList = (List<SectionForm>) DatabaseUtil.query(sql, params, new BeanListHandler(FollowForm.class));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return sectionFormList;
 	}
 
 }
--- src/com/neusoft/bbs/dao/impl/UserDetailDaoImpl.java
+++ src/com/neusoft/bbs/dao/impl/UserDetailDaoImpl.java
@@ -1,15 +1,21 @@
 package com.neusoft.bbs.dao.impl;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.neusoft.bbs.commons.util.db.BeanHandler;
 import com.neusoft.bbs.commons.util.db.BeanListHandler;
 import com.neusoft.bbs.commons.util.db.DatabaseUtil;
 import com.neusoft.bbs.dao.UserDetailDao;
+import com.neusoft.bbs.domain.EXP;
+import com.neusoft.bbs.domain.Level;
+import com.neusoft.bbs.domain.UserBase;
 import com.neusoft.bbs.domain.UserDetail;
+import com.neusoft.bbs.domain.form.UserForm;
 
 /***
  * 用户详细信息Dao实现类
+ * 
  * @author flyblue
  *
  */
@@ -19,8 +25,9 @@ public class UserDetailDaoImpl implements UserDetailDao {
 	public int insert(UserDetail userDetail) {
 		int a = 0;
 		String sql = "insert into b_user_detail values(?,?,?,?,?,to_date(?,'yyyy-mm-dd'),?,?,?)";
-		Object params[]={userDetail.getUserId(),userDetail.getIcon(),userDetail.getSex(),userDetail.getSignature(),
-						userDetail.getIntro(),userDetail.getBirthday(),userDetail.getRegion(),userDetail.getWebsite(),userDetail.getQq()};
+		Object params[] = { userDetail.getUserId(), userDetail.getIcon(), userDetail.getSex(),
+				userDetail.getSignature(), userDetail.getIntro(), userDetail.getBirthday(), userDetail.getRegion(),
+				userDetail.getWebsite(), userDetail.getQq() };
 		try {
 			a = DatabaseUtil.update(sql, params);
 		} catch (Exception e) {
@@ -33,7 +40,7 @@ public class UserDetailDaoImpl implements UserDetailDao {
 	public int delete(UserDetail userDetail) {
 		int a = 0;
 		String sql = "delete from b_user_detail where user_id=?";
-		Object params[] = {userDetail.getUserId()};
+		Object params[] = { userDetail.getUserId() };
 		try {
 			a = DatabaseUtil.update(sql, params);
 		} catch (Exception e) {
@@ -47,9 +54,9 @@ public class UserDetailDaoImpl implements UserDetailDao {
 		int a = 0;
 		String sql = "update b_user_detail set user_id=?,icon=?,sex=?,signature=?"
 				+ ",intro=?,birthday=to_date(?,'yyyy-mm-dd'),region=?,website=?,qq=? where user_id=?";
-		Object params[] = {userDetail.getUserId(),userDetail.getIcon(),userDetail.getSex(),userDetail.getSignature(),
-				userDetail.getIntro(),userDetail.getBirthday(),userDetail.getRegion(),userDetail.getWebsite(),
-				userDetail.getQq(),userDetail.getUserId()};
+		Object params[] = { userDetail.getUserId(), userDetail.getIcon(), userDetail.getSex(),
+				userDetail.getSignature(), userDetail.getIntro(), userDetail.getBirthday(), userDetail.getRegion(),
+				userDetail.getWebsite(), userDetail.getQq(), userDetail.getUserId() };
 		try {
 			a = DatabaseUtil.update(sql, params);
 		} catch (Exception e) {
@@ -61,11 +68,11 @@ public class UserDetailDaoImpl implements UserDetailDao {
 	@Override
 	public UserDetail findByUserId(Long userId) {
 		String sql = "select * from b_user_detail where user_id=?";
-		Object params[] = {userId};
+		Object params[] = { userId };
 		UserDetail userDetail = null;
-		
+
 		try {
-			userDetail = (UserDetail)DatabaseUtil.query(sql, params, new BeanHandler(UserDetail.class));
+			userDetail = (UserDetail) DatabaseUtil.query(sql, params, new BeanHandler(UserDetail.class));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -75,15 +82,31 @@ public class UserDetailDaoImpl implements UserDetailDao {
 	@Override
 	public List<UserDetail> findAll() {
 		String sql = "select * from b_user_detail where user_id=?";
-		 List<UserDetail> list = null;
+		List<UserDetail> list = null;
 		Object params[] = {};
 		try {
-			list = (List<UserDetail>)DatabaseUtil.query(sql, params, new BeanListHandler(UserDetail.class));
+			list = (List<UserDetail>) DatabaseUtil.query(sql, params, new BeanListHandler(UserDetail.class));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return list;
-		
+
+	}
+
+	@Override
+	public UserForm findUserForm(Long userId) {
+		String sql = "SELECT B.USER_ID,B.USERNAME,B.EMAIL,B.PASSWORD,B.LAST_LOGIN_TIME,B.LAST_LOGIN_IP,B.POWER,B.REGIST_TIME,D.ICON,D.SEX,D.SIGNATURE,D.INTRO,D.BIRTHDAY,D.REGION,D.WEBSITE,D.QQ,E.exp_num,L.level_name,C.coin_num "
+				+ "from b_user_base B,b_user_detail D,b_exp E,b_level L,b_coin C " + "where B.user_id=D.user_id"
+				+ " and D.user_id=E.user_id" + " and L.level_id=E.level_id" + " and E.user_id=C.user_id"
+				+ " and C.user_id=?";
+		Object params[] = { userId };
+		UserForm userForm = null;
+		try {
+			userForm = (UserForm) DatabaseUtil.query(sql, params, new BeanHandler(UserForm.class));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userForm;
 	}
 
 }
