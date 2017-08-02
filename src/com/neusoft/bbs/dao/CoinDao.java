abcdf3c90868f139133d6a0ba07b3e16c69c7136
flyblue <838797517@qq.com>
2017-08-02 14:34:51 +0800

    CoinDao、ExpDao接口更新

--- src/com/neusoft/bbs/dao/CoinDao.java
+++ src/com/neusoft/bbs/dao/CoinDao.java
@@ -12,47 +12,69 @@ import com.neusoft.bbs.domain.CoinRecord;
  */
 public interface CoinDao {
 	
-	
 	/**
-	 * 根据用户id查询金币记录
-	 * @param userId
+	 * 根据coinId查询金币记录
+	 * @param coinId
 	 * @return
 	 */
 	public List<CoinRecord> findCoinRecordByCoinId(Long coinId);
+	
 	/**
 	 * 根据用户id删除金币记录
 	 * @param userId
 	 * @return
 	 */
 	public int deleteCoinRecord(Long userId);
+	
 	/**
 	 * 根据用户id插入记录
 	 * @param userId
 	 * @return
 	 */
 	public int insertCoinRecord(CoinRecord coinRecord);
+	
 	/**
 	 * 根据用户id修改数据
 	 * @param userId
 	 * @return
 	 */
 	public int updateCoinRecord(CoinRecord coinRecord);
+	
 	/**
 	 * 获取每个用户的金币数
 	 * @param userId
 	 * @return
 	 */
 	public Coin findCoinNum(Long userId);
+	
 	/**
 	 * 修改金币总数
 	 * @param userId
 	 * @return
 	 */
 	public int updateCoinNum(Coin coin);
-	/**
-	 * 根据coinId查出所有金币记录
-	 * @param coinId
+	
+	/***
+	 * 获取最大页数
+	 * @param pageSize，每页显示信息条数
+	 * @param userId，查询条件
 	 * @return
 	 */
-	public CoinRecord findCoinRecord(Long coinId);
+	int getListPageCount(int pageSize, Long userId);
+
+	/***
+	 * 获取最大行数
+	 * @param userId，查询条件（帖子postId查和跟帖用户commentUserId查）
+	 * @return
+	 */
+	int getListRowCount(Long userId);
+	
+	/***
+	 * 查找金币记录
+	 * @param pageSize，每页显示信息条数
+	 * @param rowNum，需要获取的页数
+	 * @param userId，查询条件（userId）
+	 * @return
+	 */
+	List<CoinRecord> findFormList(int pageSize, int rowNum, Long userId);
 }
--- src/com/neusoft/bbs/dao/ExpDao.java
+++ src/com/neusoft/bbs/dao/ExpDao.java
@@ -3,12 +3,13 @@ package com.neusoft.bbs.dao;
 import java.util.List;
 
 import com.neusoft.bbs.domain.EXP;
+import com.neusoft.bbs.domain.ExpRecord;
+
 /**
  * 经验DAO接口
  * @author Tony
  *
  */
-import com.neusoft.bbs.domain.ExpRecord;
 public interface ExpDao {
 	/**
 	 * 通过userId查询EXP
@@ -16,30 +17,35 @@ public interface ExpDao {
 	 * @return EXP
 	 */
 	EXP findExpById(Long user_Id);
+	
 	/**
 	 * 更新EXP表
 	 * @param exp
 	 * @return
 	 */
 	int updateExp(EXP exp);
+	
 	/**
 	 * 插入数据
 	 * @param exp
 	 * @return
 	 */
 	int insertExp(EXP exp);
+	
 	/**
 	 * 根据userId插入经验记录
 	 * @param expRecord
 	 * @return
 	 */
 	int insertExpRecord(Long userId,ExpRecord expRecord);
+	
 	/**
 	 * 根据userId更新经验记录
 	 * @param expRecord
 	 * @return
 	 */
-	int updateExpRecord(Long userId,ExpRecord expRecord); 
+	int updateExpRecord(Long userId,ExpRecord expRecord);
+	
 	/**
 	 * 根据userId查找经验记录
 	 * @param expRecord
@@ -47,4 +53,27 @@ public interface ExpDao {
 	 */
 	List<ExpRecord> findExpRecord(Long userId,ExpRecord expRecord);
 	
+	/***
+	 * 获取最大页数
+	 * @param pageSize，每页显示信息条数
+	 * @param userId，查询条件
+	 * @return
+	 */
+	int getListPageCount(int pageSize, Long userId);
+
+	/***
+	 * 获取最大行数
+	 * @param userId，查询条件（帖子postId查和跟帖用户commentUserId查）
+	 * @return
+	 */
+	int getListRowCount(Long userId);
+	
+	/***
+	 * 查找经验记录
+	 * @param pageSize，每页显示信息条数
+	 * @param rowNum，需要获取的页数
+	 * @param userId，查询条件（userId）
+	 * @return
+	 */
+	List<ExpRecord> findFormList(int pageSize, int rowNum, Long userId);
 }
