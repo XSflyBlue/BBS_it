360a7f1daad5b24147beb094eeb63f87d30f75d9
cappuccinoqi <617674934@qq.com>
2017-08-07 22:46:04 +0800

    实现精华帖过有效期后更改IsElite为0，并移除返回的list中的对应元素

--- src/com/neusoft/bbs/service/impl/PostServiceImpl.java
+++ src/com/neusoft/bbs/service/impl/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package com.neusoft.bbs.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import com.neusoft.bbs.commons.util.db.TransactionProxy;
@@ -94,7 +95,56 @@ public class PostServiceImpl implements PostService {
 	public List<PostForm> findFormList(int pageSize, int rowNum, Post post) {
 		List<PostForm> postFormList = null;
 		postFormList = postDao.findFormList(pageSize, rowNum, post);
+		if(post.getIsElite()!=null){
+			// 查询精华帖的时候才进行有效期判断
+			for(int i =0;i<postFormList.size();i++){
+				String currentDate = new Date().toLocaleString();
+				String dbDate = postFormList.get(i).getRecomValidity().toLocaleString();
+				if(currentDate.compareTo(dbDate)<0){
+					System.out.println("忽略的帖子id:"+postFormList.get(i).getPostId());
+					// 当前时间早于帖子精华有效时间，不用处理
+				}else{
+					// 帖子精华有效期已过（两者相等的话查询出来也已经过期），更改isElite字段并从返回的list中移除
+					Post tempPost = new Post();				
+					tempPost.setPostId(postFormList.get(i).getPostId());
+					tempPost.setUserId(postFormList.get(i).getUserId());
+					tempPost.setSectionId(postFormList.get(i).getSectionId());
+					tempPost.setPostType(postFormList.get(i).getPostType());
+					tempPost.setThemeContent(postFormList.get(i).getThemeContent());
+					tempPost.setIssueTime(postFormList.get(i).getIssueTime());
+					tempPost.setIssueIp(postFormList.get(i).getIssueIp());
+					tempPost.setHitNum(postFormList.get(i).getHitNum());
+					tempPost.setAnswerSum(postFormList.get(i).getAnswerSum()); 
+					tempPost.setIsHighlight(postFormList.get(i).getIsHighlight());
+					tempPost.setHighlightUserId(postFormList.get(i).getHighlightUserId());
+					tempPost.setTitleColor(postFormList.get(i).getTitleColor()); 
+					tempPost.setIsOverhead(postFormList.get(i).getIsOverhead()); 
+					tempPost.setOverheadUserId(postFormList.get(i).getOverheadUserId());
+					tempPost.setOverheadCause(postFormList.get(i).getOverheadCause()); 
+					tempPost.setIsClose(postFormList.get(i).getIsClose());
+					tempPost.setSwitchUserId(postFormList.get(i).getSwitchUserId());
+					tempPost.setSwitchCause(postFormList.get(i).getSwitchCause());
+					tempPost.setIsElite(Short.parseShort("0"));// 更改帖子精华状态
+					tempPost.setRecomUserId(postFormList.get(i).getRecomUserId());
+					// 可能需要修改PostDaoImpl的update方法
+					tempPost.setRecomValidity(postFormList.get(i).getRecomValidity());
+					tempPost.setIsHidden(postFormList.get(i).getIsHidden());
+					tempPost.setHiddenCause(postFormList.get(i).getHiddenCause());
+					tempPost.setHiddenUserId(postFormList.get(i).getHiddenUserId());
+					tempPost.setIsAccessory(postFormList.get(i).getIsAccessory());
+					tempPost.setEditUserId(postFormList.get(i).getEditUserId());
+					tempPost.setEditTime(postFormList.get(i).getEditTime());
+					tempPost.setPostTitle(postFormList.get(i).getPostTitle());
+					
+					// 更新已过期的精华帖状态
+					postDao.update(tempPost);
+					// 从结果列表移除
+					postFormList.remove(i);	
+					// 移除当前元素之后重新从当前位置继续遍历
+					i--;
+				}
+			}
+		}
 		return postFormList;
 	}
-
 }
